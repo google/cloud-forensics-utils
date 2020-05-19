55faa0c67f240042e5c24da16a84a3fdb94146ae
Theo <gtheo@google.com>
2020-05-19 15:47:43 +0200

    Initial refactor proposal (#93)
    Signed-off-by: Theo Giovanna <gtheo@google.com>

---
+++ libcloudforensics/providers/aws/internal/ebs.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Disk functionality."""
+
+import datetime
+
+import botocore
+
+from libcloudforensics.providers.aws.internal.common import EC2_SERVICE, \
+  REGEX_TAG_VALUE, GetTagForResourceType
+
+
+class AWSElasticBlockStore:
+  """Class representing an AWS EBS resource.
+
+  Attributes:
+    aws_account (AWSAccount): The account for the resource.
+    region (str): The region the EBS is in.
+    availability_zone (str): The zone within the region in which the EBS is.
+    encrypted (bool): True if the EBS resource is encrypted, False otherwise.
+    name (str): The name tag of the EBS resource, if existing.
+  """
+
+  def __init__(self,
+               aws_account,
+               region,
+               availability_zone,
+               encrypted,
+               name=None):
+    """Initialize the AWS EBS resource.
+
+    Args:
+      aws_account (AWSAccount): The account for the resource.
+      region (str): The region the EBS is in.
+      availability_zone (str): The zone within the region in which the EBS is.
+      encrypted (bool): True if the EBS resource is encrypted, False otherwise.
+      name (str): Optional. The name tag of the EBS resource, if existing.
+    """
+
+    self.aws_account = aws_account
+    self.region = region
+    self.availability_zone = availability_zone
+    self.encrypted = encrypted
+    self.name = name
+
+
+class AWSVolume(AWSElasticBlockStore):
+  """Class representing an AWS EBS volume.
+
+  Attributes:
+    volume_id (str): The id of the volume.
+    aws_account (AWSAccount): The account for the volume.
+    region (str): The region the volume is in.
+    availability_zone (str): The zone within the region in which the volume is.
+    encrypted (bool): True if the volume is encrypted, False otherwise.
+    name (str): The name tag of the volume, if existing.
+    device_name (str): The device name (e.g. /dev/spf) of the
+        volume when it is attached to an instance, if applicable.
+  """
+
+  def __init__(self,
+               volume_id,
+               aws_account,
+               region,
+               availability_zone,
+               encrypted,
+               name=None,
+               device_name=None):
+    """Initialize an AWS EBS volume.
+
+    Args:
+      volume_id (str): The id of the volume.
+      aws_account (AWSAccount): The account for the volume.
+      region (str): The region the volume is in.
+      availability_zone (str): The zone within the region in which the volume
+          is.
+      encrypted (bool): True if the volume is encrypted, False otherwise.
+      name (str): Optional. The name tag of the volume, if existing.
+      device_name (str): Optional. The device name (e.g. /dev/spf) of the
+          volume when it is attached to an instance, if applicable.
+    """
+
+    super(AWSVolume, self).__init__(aws_account,
+                                    region,
+                                    availability_zone,
+                                    encrypted,
+                                    name)
+    self.volume_id = volume_id
+    self.device_name = device_name
+
+  def Snapshot(self, snapshot_name=None):
+    """Create a snapshot of the volume.
+
+    Args:
+      snapshot_name (str): Optional. Name tag of the snapshot.
+
+    Returns:
+      AWSSnapshot: A snapshot object.
+
+    Raises:
+      ValueError: If the snapshot name does not comply with the RegEx.
+      RuntimeError: If the snapshot could not be created.
+    """
+
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    if not snapshot_name:
+      snapshot_name = self.volume_id
+    truncate_at = 255 - len(timestamp) - 1
+    snapshot_name = '{0}-{1}'.format(snapshot_name[:truncate_at], timestamp)
+    if not REGEX_TAG_VALUE.match(snapshot_name):
+      raise ValueError('Snapshot name {0:s} does not comply with '
+                       '{1:s}'.format(snapshot_name, REGEX_TAG_VALUE.pattern))
+
+    client = self.aws_account.ClientApi(EC2_SERVICE)
+    try:
+      snapshot = client.create_snapshot(
+          VolumeId=self.volume_id,
+          TagSpecifications=[GetTagForResourceType('snapshot', snapshot_name)])
+
+      snapshot_id = snapshot.get('SnapshotId')
+      # Wait for snapshot completion
+      client.get_waiter('snapshot_completed').wait(SnapshotIds=[snapshot_id])
+    except (client.exceptions.ClientError,
+            botocore.exceptions.WaiterError) as exception:
+      raise RuntimeError('Could not create snapshot for volume {0:s}: '
+                         '{1:s}'.format(self.volume_id, str(exception)))
+
+    return AWSSnapshot(snapshot_id, self, name=snapshot_name)
+
+  def Delete(self):
+    """Delete a volume."""
+    client = self.aws_account.ClientApi(EC2_SERVICE)
+    try:
+      client.delete_volume(VolumeId=self.volume_id)
+    except client.exceptions.ClientError as exception:
+      raise RuntimeError('Could not delete volume {0:s}: {1:s}'.format(
+          self.volume_id, str(exception)))
+
+
+class AWSSnapshot(AWSElasticBlockStore):
+  """Class representing an AWS EBS snapshot.
+
+  Attributes:
+    snapshot_id (str): The id of the snapshot.
+    volume (AWSVolume): The volume from which the snapshot was taken.
+    name (str): The name tag of the snapshot, if existing.
+  """
+
+  def __init__(self, snapshot_id, volume, name=None):
+    """Initialize an AWS EBS snapshot.
+
+    Args:
+      snapshot_id (str): The id of the snapshot.
+      volume (AWSVolume): The volume from which the snapshot was taken.
+      name (str): Optional. The name tag of the snapshot, if existing.
+    """
+
+    super(AWSSnapshot, self).__init__(volume.aws_account,
+                                      volume.region,
+                                      volume.availability_zone,
+                                      volume.encrypted,
+                                      name)
+    self.snapshot_id = snapshot_id
+    self.volume = volume
+
+  def Delete(self):
+    """Delete a snapshot."""
+
+    client = self.aws_account.ClientApi(EC2_SERVICE)
+    try:
+      client.delete_snapshot(SnapshotId=self.snapshot_id)
+    except client.exceptions.ClientError as exception:
+      raise RuntimeError('Could not delete snapshot {0:s}: {1:s}'.format(
+          self.snapshot_id, str(exception)))
+
+  def ShareWithAWSAccount(self, aws_account_id):
+    """Share the snapshot with another AWS account ID.
+
+    Args:
+      aws_account_id (str): The AWS Account ID to share the snapshot with.
+    """
+
+    snapshot = self.aws_account.ResourceApi(EC2_SERVICE).Snapshot(
+        self.snapshot_id)
+    snapshot.modify_attribute(
+        Attribute='createVolumePermission',
+        CreateVolumePermission={
+            'Add': [{
+                'UserId': aws_account_id
+            }]
+        },
+        OperationType='add'
+    )
---
+++ libcloudforensics/providers/gcp/internal/log.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Log functionality."""
+
+import time
+
+from google.auth.exceptions import RefreshError, DefaultCredentialsError
+
+from libcloudforensics.providers.gcp.internal.common import CreateService
+
+
+class GoogleCloudLog:
+  """Class representing a Google Cloud Logs interface.
+
+  Attributes:
+    project_id: Project name.
+    gcl_api_client: Client to interact with GCP logging API.
+
+  Example use:
+    # pylint: disable=line-too-long
+    gcp = GoogleCloudLog(project_id='your_project_name')
+    gcp.ListLogs()
+    gcp.ExecuteQuery(filter='resource.type="gce_instance" labels."compute.googleapis.com/resource_name"="instance-1"')
+    See https://cloud.google.com/logging/docs/view/advanced-queries for filter details.
+  """
+
+  LOGGING_API_VERSION = 'v2'
+
+  def __init__(self, project_id):
+    """Initialize the GoogleCloudProject object.
+
+    Args:
+      project_id (str): The name of the project.
+    """
+
+    self.project_id = project_id
+    self.gcl_api_client = None
+
+  def GclApi(self):
+    """Get a Google Compute Logging service object.
+
+    Returns:
+      apiclient.discovery.Resource: A Google Compute Logging service object.
+    """
+
+    if self.gcl_api_client:
+      return self.gcl_api_client
+    self.gcl_api_client = CreateService('logging', self.LOGGING_API_VERSION)
+    return self.gcl_api_client
+
+  def ListLogs(self):
+    """List logs in project.
+
+    Returns:
+      list: The project logs available.
+
+    Raises:
+      RuntimeError: If API call failed.
+    """
+
+    have_all_tokens = False
+    page_token = None
+    logs = []
+    while not have_all_tokens:
+      gcl_instance_client = self.GclApi().logs()
+      if page_token:
+        request = gcl_instance_client.list(
+            parent=self.project_id, pageToken=page_token)
+      else:
+        request = gcl_instance_client.list(parent='projects/' + self.project_id)
+      try:
+        result = request.execute()
+      except (RefreshError, DefaultCredentialsError) as exception:
+        error_msg = (
+            '{0:s}\n'
+            'Something is wrong with your Application Default '
+            'Credentials. Try running: '
+            '$ gcloud auth application-default login'.format(str(exception)))
+        raise RuntimeError(error_msg)
+      for logtypes in result.get('logNames', []):
+        logs.append(logtypes)
+      page_token = result.get('nextPageToken')
+      if not page_token:
+        have_all_tokens = True
+
+    return logs
+
+  def ExecuteQuery(self, qfilter):
+    """Query logs in GCP project.
+
+    Args:
+      qfilter (str): The query filter to use.
+
+    Returns:
+      list(dict): Log entries returned by the query.
+
+    Raises:
+      RuntimeError: If API call failed.
+    """
+
+    body = {
+        'resourceNames': 'projects/' + self.project_id,
+        'filter': qfilter,
+        'orderBy': 'timestamp desc',
+    }
+
+    have_all_tokens = False
+    page_token = None
+    entries = []
+    while not have_all_tokens:
+      gcl_instance_client = self.GclApi().entries()
+      if page_token:
+        # This sleep is needed as the API rate limits. It will *not* speed
+        # up the query by asking if there are new results more frequently.
+        time.sleep(1)
+        body['pageToken'] = page_token
+        request = gcl_instance_client.list(body=body)
+      else:
+        request = gcl_instance_client.list(body=body)
+      try:
+        result = request.execute()
+      except (RefreshError, DefaultCredentialsError) as exception:
+        error_msg = (
+            '{0:s}\n'
+            'Something is wrong with your Application Default '
+            'Credentials. Try running: '
+            '$ gcloud auth application-default login'.format(str(exception)))
+        raise RuntimeError(error_msg)
+      for entry in result.get('entries', []):
+        entries.append(entry)
+      page_token = result.get('nextPageToken')
+      if not page_token:
+        have_all_tokens = True
+
+    return entries
