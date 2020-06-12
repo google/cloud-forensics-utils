8fac93f028d10aa739d5fa44a5f1490c31864b36
Theo Giovanna <gtheo@google.com>
2020-06-12 16:12:01 +0200

    Add Azure snapshotting capabilities
    Signed-off-by: Theo Giovanna <gtheo@google.com>

---
+++ libcloudforensics/providers/azure/forensics.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Forensics on Azure."""
+
+from typing import TYPE_CHECKING, Optional
+
+from libcloudforensics.providers.azure.internal import account
+from libcloudforensics.providers.azure.internal import common
+
+if TYPE_CHECKING:
+  from libcloudforensics.providers.azure.internal import compute
+
+
+def CreateDiskCopy(
+    subscription_id: str,
+    instance_name: str,
+    disk_name: Optional[str] = None,
+    disk_type: Optional[str] = 'Standard_LRS') -> 'compute.AZDisk':
+  """Creates a copy of an Azure Compute Disk.
+
+  Args:
+    subscription_id (str): The Azure subscription ID to use.
+    instance_name (str): Instance name using the disk to be copied.
+    disk_name (str): Optional. Name of the disk to copy. If None, boot disk
+        will be copied.
+    disk_type (str): Optional. The sku name for the disk to create. Can be
+          Standard_LRS, Premium_LRS, StandardSSD_LRS, or UltraSSD_LRS.
+
+  Returns:
+    GoogleComputeDisk: A Google Compute Disk object.
+
+  Raises:
+    RuntimeError: If there are errors copying the disk
+  """
+
+  az_account = account.AZAccount(subscription_id)
+  instance = az_account.GetInstance(instance_name) if instance_name else None
+
+  try:
+    if disk_name:
+      disk_to_copy = az_account.GetDisk(disk_name)
+    else:
+      disk_to_copy = instance.GetBootDisk()  # type: ignore
+    common.LOGGER.info('Disk copy of {0:s} started...'.format(
+        disk_to_copy.name))
+    snapshot = disk_to_copy.Snapshot()
+    new_disk = az_account.CreateDiskFromSnapshot(
+        snapshot, disk_name_prefix='evidence', disk_type=disk_type)
+    snapshot.Delete()
+    common.LOGGER.info(
+        'Disk {0:s} successfully copied to {1:s}'.format(
+            disk_to_copy.name, new_disk.name))
+  except RuntimeError as exception:
+    error_msg = 'Cannot copy disk "{0:s}": {1!s}'.format(disk_name, exception)
+    raise RuntimeError(error_msg)
+
+  return new_disk
---
+++ libcloudforensics/providers/azure/internal/account.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Represents an Azure account."""
+
+from time import sleep
+from typing import Optional, Dict
+
+# pylint: disable=import-error
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.compute.v2016_04_30_preview.models import DiskCreateOption
+from msrestazure.azure_exceptions import CloudError
+# pylint: enable=import-error
+
+from libcloudforensics.providers.azure.internal import compute, common
+
+
+class AZAccount:
+  """Class that represents an Azure Account.
+
+  Attributes:
+    subscription_id (str): The Azure subscription ID to use.
+    credentials (ServicePrincipalCredentials): An Azure credentials object.
+    compute_client (ComputeManagementClient): An Azure compute client object.
+  """
+
+  def __init__(self, subscription_id: str) -> None:
+    """Initialize the AZAccount class.
+
+    Args:
+      subscription_id (str): The Azure subscription ID to use.
+    """
+    self.subscription_id = subscription_id
+    self.credentials = common.GetCredentials()
+    self.compute_client = ComputeManagementClient(
+        self.credentials, self.subscription_id)
+
+  def ListInstances(
+      self,
+      # pylint: disable=line-too-long
+      resource_group_name: Optional[str] = None) -> Dict[str, compute.AZVirtualMachine]:
+      # pylint: enable=line-too-long
+    """List instances in an Azure subscription / resource group.
+
+    Args:
+      resource_group_name (str): Optional. The resource group name to list
+          instances from. If none specified, then all instances in the AZ
+          subscription will be listed.
+
+    Returns:
+      Dict[str, AZCompute]: Dictionary mapping instance names (str)
+          to their respective AZCompute object.
+    """
+    instances = {}  # type: Dict[str, compute.AZVirtualMachine]
+    az_vm_client = self.compute_client.virtual_machines
+    if not resource_group_name:
+      responses = common.ExecuteRequest(az_vm_client, 'list_all')
+    else:
+      responses = common.ExecuteRequest(
+          az_vm_client,
+          'list',
+          {'resource_group_name': resource_group_name})  # type: ignore
+    for response in responses:
+      for instance in response:
+        vm_name = instance.name
+        instances[vm_name] = compute.AZVirtualMachine(self,
+                                                      instance.id,
+                                                      vm_name,
+                                                      instance.location,
+                                                      zones=instance.zones)
+    return instances
+
+  def ListDisks(
+      self,
+      # pylint: disable=line-too-long
+      resource_group_name: Optional[str] = None) -> Dict[str, compute.AZDisk]:
+      # pylint: enable=line-too-long
+    """List instances in an Azure subscription / resource group.
+
+    Args:
+      resource_group_name (str): Optional. The resource group name to list
+          disks from. If none specified, then all disks in the AZ
+          subscription will be listed.
+
+    Returns:
+      Dict[str, AZDisk]: Dictionary mapping instance names (str)
+          to their respective AZDisk object.
+    """
+    disks = {}  # type: Dict[str, compute.AZDisk]
+    az_disk_client = self.compute_client.disks
+    if not resource_group_name:
+      responses = common.ExecuteRequest(az_disk_client, 'list')
+    else:
+      responses = common.ExecuteRequest(
+          az_disk_client,
+          'list_by_resource_group',
+          {'resource_group_name': resource_group_name})  # type: ignore
+    for response in responses:
+      for disk in response:
+        disk_name = disk.name
+        disks[disk_name] = compute.AZDisk(self,
+                                          disk.id,
+                                          disk_name,
+                                          disk.location,
+                                          zones=disk.zones)
+    return disks
+
+  def GetInstance(
+      self,
+      instance_name: str,
+      resource_group_name: Optional[str] = None) -> compute.AZVirtualMachine:
+    """Get instance from AZ subscription / resource group.
+
+    Args:
+      instance_name (str): The instance name.
+      resource_group_name (str): Optional. The resource group name to look
+          the instance in. If none specified, then the instance will be fetched
+          from the AZ subscription.
+
+    Returns:
+      AZCompute: An Azure Compute Instance object.
+
+    Raises:
+      RuntimeError: If the instance was not found in the subscription / resource
+          group.
+    """
+    instances = self.ListInstances(resource_group_name=resource_group_name)
+    if instance_name not in instances:
+      error_msg = 'Instance {0:s} was not found in subscription {1:s}'.format(
+          instance_name, self.subscription_id)
+      raise RuntimeError(error_msg)
+    return instances[instance_name]
+
+  def GetDisk(
+      self,
+      disk_name: str,
+      resource_group_name: Optional[str] = None) -> compute.AZDisk:
+    """Get disk from AZ subscription / resource group.
+
+    Args:
+      disk_name (str): The disk name.
+      resource_group_name (str): Optional. The resource group name to look
+          the disk in. If none specified, then the disk will be fetched from
+          the AZ subscription.
+
+    Returns:
+      AZDisk: An Azure Compute Disk object.
+
+    Raises:
+      RuntimeError: If the disk was not found in the subscription / resource
+          group.
+    """
+    disks = self.ListDisks(resource_group_name=resource_group_name)
+    if disk_name not in disks:
+      error_msg = 'Disk {0:s} was not found in subscription {1:s}'.format(
+          disk_name, self.subscription_id)
+      raise RuntimeError(error_msg)
+    return disks[disk_name]
+
+  def CreateDiskFromSnapshot(
+      self,
+      snapshot: compute.AZSnapshot,
+      disk_name: Optional[str] = None,
+      disk_name_prefix: Optional[str] = '',
+      disk_type: Optional[str] = 'Standard_LRS') -> compute.AZDisk:
+    """Create a new disk based on a Snapshot.
+
+    Args:
+      snapshot (AZSnapshot): Snapshot to use.
+      disk_name (str): Optional. String to use as new disk name.
+      disk_name_prefix (str): Optional. String to prefix the disk name with.
+      disk_type (str): Optional. The sku name for the disk to create. Can be
+          Standard_LRS, Premium_LRS, StandardSSD_LRS, or UltraSSD_LRS.
+
+    Returns:
+      AZDisk: Azure Compute Disk.
+
+    Raises:
+      RuntimeError: If the disk exists already.
+    """
+
+    if not disk_name:
+      disk_name = common.GenerateDiskName(snapshot,
+                                          disk_name_prefix=disk_name_prefix)
+    creation_data = {
+        'location': snapshot.region,
+        'creation_data': {
+            'sourceResourceId': snapshot.resource_id,
+            'create_option': DiskCreateOption.copy
+        }
+    }
+
+    try:
+      request = self.compute_client.disks.create_or_update(
+          snapshot.resource_group_name,
+          disk_name,
+          creation_data,
+          sku=disk_type)
+      while not request.done():
+        sleep(5)  # Wait 5 seconds before checking disk status again
+      disk = request.result()
+    except CloudError as exception:
+      raise RuntimeError('Could not create disk from snapshot {0:s}: {1:s}'
+                         .format(snapshot.resource_id, str(exception)))
+
+    return compute.AZDisk(self,
+                          disk.id,
+                          disk.name,
+                          disk.location,
+                          disk.zones)
