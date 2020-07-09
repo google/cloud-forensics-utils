3515530cb97e9011f80649df60b37cadaa4f00ac
Theo Giovanna <gtheo@google.com>
2020-07-09 13:43:32 +0200

    Fix unittests
    Signed-off-by: Theo Giovanna <gtheo@google.com>

--- tests/providers/azure/azure_test.py
+++ tests/providers/azure/azure_test.py
@@ -20,9 +20,13 @@ import mock
 from libcloudforensics.providers.azure.internal import account, compute, common
 from libcloudforensics.providers.azure import forensics
 
-FAKE_ACCOUNT = account.AZAccount(
-    'fake-subscription-id'
-)
+# pylint: disable=line-too-long
+with mock.patch('libcloudforensics.providers.azure.internal.common.GetCredentials') as mock_creds:
+  # pylint: enable=line-too-long
+  mock_creds.return_value = mock.Mock()
+  FAKE_ACCOUNT = account.AZAccount(
+      'fake-subscription-id'
+  )
 
 FAKE_INSTANCE = compute.AZVirtualMachine(
     FAKE_ACCOUNT,
@@ -316,6 +320,7 @@ class TestForensics(unittest.TestCase):
   """Test Azure forensics file."""
   # pylint: disable=line-too-long
 
+  @mock.patch('libcloudforensics.providers.azure.internal.common.GetCredentials')
   @mock.patch('libcloudforensics.providers.azure.internal.compute.AZSnapshot.Delete')
   @mock.patch('libcloudforensics.providers.azure.internal.compute.AZDisk.Snapshot')
   @mock.patch('libcloudforensics.providers.azure.internal.account.AZAccount.GetDisk')
@@ -329,7 +334,8 @@ class TestForensics(unittest.TestCase):
                           mock_get_boot_disk,
                           mock_get_disk,
                           mock_snapshot,
-                          mock_snapshot_delete):
+                          mock_snapshot_delete,
+                          mock_credentials):
     """Test that a disk copy is correctly created."""
     mock_create_disk.return_value.done.return_value = True
     mock_create_disk.return_value.result.return_value = MOCK_DISK_COPY
@@ -337,6 +343,7 @@ class TestForensics(unittest.TestCase):
     mock_get_boot_disk.return_value = FAKE_BOOT_DISK
     mock_snapshot.return_value = FAKE_SNAPSHOT
     mock_snapshot_delete.return_value = None
+    mock_credentials.return_value = mock.Mock()
 
     # CreateDiskCopy(zone, instance_name='fake-vm-name'). This should grab
     # the boot disk of the instance.
@@ -349,6 +356,7 @@ class TestForensics(unittest.TestCase):
     self.assertIsInstance(disk_copy, compute.AZDisk)
     self.assertEqual('fake_snapshot_name_f4c186ac_copy', disk_copy.name)
 
+  @mock.patch('libcloudforensics.providers.azure.internal.common.GetCredentials')
   @mock.patch('libcloudforensics.providers.azure.internal.compute.AZSnapshot.Delete')
   @mock.patch('libcloudforensics.providers.azure.internal.compute.AZDisk.Snapshot')
   @mock.patch('libcloudforensics.providers.azure.internal.account.AZAccount.GetDisk')
@@ -362,7 +370,8 @@ class TestForensics(unittest.TestCase):
                           mock_get_boot_disk,
                           mock_get_disk,
                           mock_snapshot,
-                          mock_snapshot_delete):
+                          mock_snapshot_delete,
+                          mock_credentials):
     """Test that a disk copy is correctly created."""
     mock_create_disk.return_value.done.return_value = True
     mock_create_disk.return_value.result.return_value = MOCK_DISK_COPY
@@ -370,6 +379,7 @@ class TestForensics(unittest.TestCase):
     mock_get_disk.return_value = FAKE_DISK
     mock_snapshot.return_value = FAKE_SNAPSHOT
     mock_snapshot_delete.return_value = None
+    mock_credentials.return_value = mock.Mock()
 
     # CreateDiskCopy(zone, disk_name='fake-disk-name'). This should grab
     # the disk 'fake-disk-name'.
@@ -382,13 +392,18 @@ class TestForensics(unittest.TestCase):
     self.assertIsInstance(disk_copy, compute.AZDisk)
     self.assertEqual('fake_snapshot_name_f4c186ac_copy', disk_copy.name)
 
+  @mock.patch('libcloudforensics.providers.azure.internal.common.GetCredentials')
   @mock.patch('libcloudforensics.providers.azure.internal.account.AZAccount.ListDisks')
   @mock.patch('libcloudforensics.providers.azure.internal.account.AZAccount.ListInstances')
   @typing.no_type_check
-  def testCreateDiskCopy3(self, mock_list_instances, mock_list_disk):
+  def testCreateDiskCopy3(self,
+                          mock_list_instances,
+                          mock_list_disk,
+                          mock_credentials):
     """Test that a disk copy is correctly created."""
     mock_list_instances.return_value = {}
     mock_list_disk.return_value = {}
+    mock_credentials.return_value = mock.Mock()
 
     # Should raise a RuntimeError in GetInstance as we are querying a
     # non-existent instance.
