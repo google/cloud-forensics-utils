8131c2898f26295d2cec93383a6dd986cb917972
Theo Giovanna <gtheo@google.com>
2020-07-14 00:35:20 +0200

    Add Azure Monitoring metrics with CLI
    Signed-off-by: Theo Giovanna <gtheo@google.com>

--- examples/az_cli.py
+++ examples/az_cli.py
@@ -14,10 +14,11 @@
 # limitations under the License.
 """Demo CLI tool for Azure."""
 
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 from libcloudforensics import logging_utils
-from libcloudforensics.providers.azure.internal import account
+from libcloudforensics.providers.azure.internal import account, monitoring
 from libcloudforensics.providers.azure import forensics
 
 logging_utils.SetUpLogger(__name__)
@@ -74,3 +75,49 @@ def CreateDiskCopy(args: 'argparse.Namespace') -> None:
                                        src_profile=args.src_profile,
                                        dst_profile=args.dst_profile)
   logger.info('Done! Disk {0:s} successfully created.'.format(disk_copy.name))
+
+
+def ListMetrics(args: 'argparse.Namespace') -> None:
+  """List Azure Monitoring metrics for a resource.
+
+  Args:
+    args (argparse.Namespace): Arguments from ArgumentParser.
+  """
+  az_account = account.AZAccount(args.default_resource_group_name)
+  az_monitoring = monitoring.AZMonitoring(az_account)
+  metrics = az_monitoring.ListAvailableMetricsForResource(args.resource_id)
+  for metric in metrics:
+    logger.info('Available metric: {0:s}'.format(metric))
+
+
+def QueryMetrics(args: 'argparse.Namespace') -> None:
+  """Query Azure Monitoring metrics for a resource.
+
+  Args:
+    args (argparse.Namespace): Arguments from ArgumentParser.
+
+  Raises:
+    RuntimeError: If from_date or to_date could not be parsed.
+  """
+  az_account = account.AZAccount(args.default_resource_group_name)
+  az_monitoring = monitoring.AZMonitoring(az_account)
+  from_date, to_date = args.from_date, args.to_date
+  if from_date and to_date:
+    try:
+      from_date = datetime.strptime(from_date, '%Y-%m-%dT%H:%M:%SZ')
+      to_date = datetime.strptime(to_date, '%Y-%m-%dT%H:%M:%SZ')
+    except ValueError as exception:
+      raise RuntimeError('Cannot parse date: {0:s}'.format(str(exception)))
+  metrics = az_monitoring.GetMetricsForResource(
+      args.resource_id,
+      metrics=args.metrics,
+      from_date=from_date,
+      to_date=to_date,
+      interval=args.interval,
+      aggregation=args.aggregation or 'Total',
+      qfilter=args.qfilter)
+
+  for metric in metrics:
+    logger.info('Metric: {0:s}'.format(metric))
+    for value in metrics[metric]:
+      logger.info('Value: {0:s}'.format(value))
---
+++ libcloudforensics/providers/azure/internal/monitoring.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Azure Monitoring functionality."""
+
+from typing import List, Optional, Dict, TYPE_CHECKING
+
+# Pylint complains about the import but the library imports just fine,
+# so we can ignore the warning.
+# pylint: disable=import-error
+from azure.mgmt.monitor import MonitorManagementClient
+# pylint: enable=import-error
+from libcloudforensics.providers.azure.internal import account
+
+if TYPE_CHECKING:
+  from datetime import datetime
+
+
+class AZMonitoring:
+  """Azure Monitoring.
+
+  Attributes:
+    az_account (AZAccount): An Azure account object.
+    monitoring_client (MonitorManagementClient): An Azure monitoring client
+        object.
+  """
+
+  def __init__(self,
+               az_account: account.AZAccount) -> None:
+    """Initialize the Azure monitoring class.
+
+    Args:
+      az_account (AZAccount): An Azure account object.
+    """
+    self.az_account = az_account
+    self.monitoring_client = MonitorManagementClient(
+        self.az_account.credentials, self.az_account.subscription_id)
+
+  def ListAvailableMetricsForResource(self, resource_id: str) -> List[str]:
+    """List the available metrics for a given resource.
+
+      Args:
+        resource_id (str): The resource ID from which to list available
+            metrics.
+
+      Returns:
+        List[str]: A list of metrics that can be queried for the resource ID.
+    """
+    return [metric.name.value for metric
+            in self.monitoring_client.metric_definitions.list(resource_id)]
+
+  def GetMetricsForResource(
+      self,
+      resource_id: str,
+      metrics: str,
+      from_date: Optional['datetime'] = None,
+      to_date: Optional['datetime'] = None,
+      interval: Optional[str] = None,
+      aggregation: str = 'Total',
+      qfilter: Optional[str] = None) -> Dict[str, List[str]]:
+    """Retrieve metrics for a given resource.
+
+    Args:
+      resource_id (str): The resource ID for which to lookup the metric.
+      metrics (str): A comma separated list of metrics to retrieve. E.g.
+          'Percentage CPU,Network In'.
+      from_date (datetime.datetime): Optional. A start date from which to get
+          the metric. If passed, toDate is also required.
+      to_date (datetime.datetime): Optional. An end date until which to get the
+          metric. If passed, fromDate is also required.
+      interval (str): An interval for the metrics, e.g. 'PT1H' will output
+          metric's values with one hour granularity.
+      aggregation (str): Optional. The type of aggregation for the metric's
+          values. Default is 'Total'. Possible values: 'Total', 'Average'.
+          Both can be retrieved if passed as a single string, separated by a
+          comma.
+      qfilter (str): Optional. A filter for the query. Reduces the set of data
+          collected. If present it must contain a list of metric names to
+         retrieve of the form: *(name.value eq 'metricName' [or name.value eq
+         'metricName' or ...])*. Optionally, the filter can contain conditions
+         for the following attributes *aggregationType*, *startTime*,
+         *endTime*, and *timeGrain* of the form *attributeName operator value*.
+         Where operator is one of *ne*, *eq*, *gt*, *lt*. Several conditions
+         can be combined with parentheses and logical operators, e.g: *and*,
+         *or*.<br>Some example filter expressions are:<br>- $filter=(name.value
+         eq 'RunsSucceeded') and aggregationType eq 'Total' and startTime eq
+         2016-02-20 and endTime eq 2016-02-21 and timeGrain eq
+         duration'PT1M', - $filter=(name.value eq 'RunsSucceeded') and
+         (aggregationType eq 'Total' or aggregationType eq 'Average') and
+         startTime eq 2016-02-20 and endTime eq 2016-02-21 and timeGrain eq
+         duration'PT1H', - $filter=(name.value eq 'ActionsCompleted' or
+         name.value eq 'RunsSucceeded') and (aggregationType eq 'Total' or
+         aggregationType eq 'Average') and startTime eq 2016-02-20 and endTime
+         eq 2016-02-21 and timeGrain eq duration'PT1M'.<br><br>**NOTE**: When a
+         metrics query comes in with multiple metrics, but with no aggregation
+         types defined, the service will pick the Primary aggregation type of
+         the first metrics to be used as the default aggregation type for all
+         the metrics.
+
+    Returns:
+      Dict[str, List[str]]]: A dictionary mapping the metric to a list of the
+          metric's values.
+    """
+    kwargs = {'metricnames': metrics, 'aggregation': aggregation}
+    if from_date and to_date:
+      timespan = '{0:s}/{1:s}'.format(from_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
+                                      to_date.strftime('%Y-%m-%dT%H:%M:%SZ'))
+      kwargs['timespan'] = timespan
+    if interval:
+      kwargs['interval'] = interval
+    metrics_data = self.monitoring_client.metrics.list(
+        resource_id, filter=qfilter, **kwargs)
+    results = {}  # type: Dict[str, List[str]]
+    for metric in metrics_data.value:
+      values = []
+      for timeserie in metric.timeseries:
+        for data in timeserie.data:
+          if data.time_stamp and data.total:
+            values.append('{0:s}: {1:s}'.format(str(data.time_stamp),
+                                                str(data.total)))
+      results[metric.name.value] = values
+    return results
