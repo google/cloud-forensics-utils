2f8ba287b9d9c2b15c821aeecc3025da9e39a8bb
Aaron Peterson <aaronp@gmail.com>
2020-11-22 21:39:12 -0800

    Use correct name

--- libcloudforensics/providers/gcp/internal/compute.py
+++ libcloudforensics/providers/gcp/internal/compute.py
@@ -826,7 +826,7 @@ class GoogleComputeInstance(compute_base_resource.GoogleComputeBaseResource):
     operation_config = {
         'mode': mode,
         'source': disk.GetSourceString(),
-        'deviceName': self.name,
+        'deviceName': disk.name,
         'boot': False,
         'autoDelete': False,
     }
