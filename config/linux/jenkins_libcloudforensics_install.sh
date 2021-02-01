612182b65d2c9bbfcc753038dfbc7bb34d59e50f
hacktobeer <ramsesdebeer@gmail.com>
2021-02-01 10:31:17 +0100

    Fix test scripts.

--- config/linux/jenkins_libcloudforensics_install.sh
+++ config/linux/jenkins_libcloudforensics_install.sh
@@ -7,4 +7,7 @@ sudo apt-get update -q
 sudo apt-get install -y python3-pip
 
 # Install libcloudforensics pinned requirements
-sudo pip3 install -r ../../requirements.txt
+sudo pip3 install -r ../../requirements-base.txt
+sudo pip3 install -r ../../requirements-aws.txt
+sudo pip3 install -r ../../requirements-azure.txt
+sudo pip3 install -r ../../requirements-gcp.txt
