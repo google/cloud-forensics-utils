c49875abf3e29190e60184b73266a51b596b6fda
Thomas Chopitea <tomchop@gmail.com>
2021-02-19 11:40:06 +0100

    Ignore installed packages (#286)

--- config/linux/jenkins_libcloudforensics_install.sh
+++ config/linux/jenkins_libcloudforensics_install.sh
@@ -7,4 +7,4 @@ sudo apt-get update -q
 sudo apt-get install -y python3-pip
 
 # Install libcloudforensics pinned requirements
-sudo pip3 install -r ../../requirements.txt
+sudo pip3 install -r ../../requirements.txt --ignore-installed
