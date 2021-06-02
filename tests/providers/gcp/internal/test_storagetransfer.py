e5a2884426961281b389c79f1dd23abbce19dd26
Fry <fryx0r@gmail.com>
2021-06-01 22:45:44 -0400

    Test & CLI

---
+++ tests/providers/gcp/internal/test_storagetransfer.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for the gcp module - storagetransfer.py"""
+
+import typing
+import unittest
+import mock
+
+from tests.providers.gcp import gcp_mocks
+
+
+class GoogleCloudStorageTransferTest(unittest.TestCase):
+  """Test Google Cloud Storage Transfer class."""
+  # pylint: disable=line-too-long
+
+  @typing.no_type_check
+  @mock.patch('boto3.session.Session._setup_loader')
+  @mock.patch('libcloudforensics.providers.gcp.internal.storagetransfer.GoogleCloudStorageTransfer.GcstApi')
+  def testS3ToGCS(self, mock_gcst_api, mock_loader):
+    """Test S3ToGCS operation."""
+    api_job_create = mock_gcst_api.return_value.transferJobs.return_value.create
+    api_job_create.return_value.execute.return_value = gcp_mocks.MOCK_STORAGE_TRANSFER_JOB
+    api_job_get = mock_gcst_api.return_value.transferOperations.return_value.list
+    api_job_get.return_value.execute.return_value = gcp_mocks.MOCK_STORAGE_TRANSFER_OPERATION
+    mock_loader.return_value = None
+    transfer_results = gcp_mocks.FAKE_GCST.S3ToGCS(
+        's3://s3_source_bucket/file.name',
+        'fake-zone-2b',
+        'gs://gcs_sink_bucket/test_path')
+    self.assertEqual(1, len(transfer_results['operations']))
+    self.assertEqual('s3_source_bucket', transfer_results['operations'][0]['metadata']['transferSpec']['awsS3DataSource']['bucketName'])
+    self.assertEqual('30', transfer_results['operations'][0]['metadata']['counters']['bytesCopiedToSink'])
