5c16d7cf63fcaec0d819049a70d461328ca82aa2
Zak Cook <zcook@google.com>
2021-09-20 15:02:31 +0200

    Refactor mocks and add some workload tests

--- tests/providers/kubernetes/k8s_mocks.py
+++ tests/providers/kubernetes/k8s_mocks.py
@@ -18,42 +18,95 @@ from typing import Optional
 from unittest import mock
 
 from libcloudforensics.providers.kubernetes import base
+from libcloudforensics.providers.kubernetes import workloads
+
+from kubernetes import client
 
 MOCK_API_CLIENT = mock.Mock()
 
-
-def MakeMockNodes(amount: int) -> mock.Mock:
-  """Make mock Kubernetes API response node list, see V1NodeList."""
-  mock_nodes = mock.Mock()
-  mock_nodes.items = []
-  for i in range(amount):
-    name = 'fake-node-{0:d}'.format(i)
-    mock_nodes.items.append(MakeMockNode(name))
-  return mock_nodes
+Labels = Dict[str, str]
 
 
-def MakeMockNode(name: str) -> mock.Mock:
-  """Make mock Kubernetes API response node, see V1Node."""
-  mock_node = mock.Mock()
-  mock_node.metadata.name = name
-  return mock_node
+def MakeV1ObjectMeta(
+    name: Optional[str] = None,
+    namespace: Optional[str] = None,
+    labels: Optional[Labels] = None) -> client.V1ObjectMeta:
+  """Make Kubernetes API response metadata, see V1ObjectMeta."""
+  return client.V1ObjectMeta(name=name, namespace=namespace, labels=labels)
 
 
-def MakeMockPod(name: Optional[str] = None,
-                namespace: Optional[str] = None,
-                node_name: Optional[str] = None,
-                labels: Optional[Dict[str, str]] = None) -> mock.Mock:
-  """Make mock Kubernetes API response pod, see V1Pod."""
-  mock_pod = mock.Mock()
-  mock_pod.metadata.name = name
-  mock_pod.metadata.namespace = namespace
-  mock_pod.metadata.labels = labels
-  mock_pod.spec.node_name = node_name
-  return mock_pod
+def MakeV1NodeList(amount: int) -> client.V1NodeList:
+  """Make Kubernetes API Node list response, see V1NodeList."""
+  items = [MakeV1Node('node-{0:d}'.format(i)) for i in range(amount)]
+  return client.V1NodeList(items=items)
 
-def MakeMockK8sPod(name: str, namespace: str, read_response: mock.Mock) -> base.K8sPod:
-  """Make mock Kubernetes API response pod, see K8sPod."""
+
+def MakeV1PodList(amount: int) -> client.V1PodList:
+  """Make Kubernetes API Pod list response, see V1PodList."""
+  items = [MakeV1Pod(name='pod-{0:d}'.format(i)) for i in range(amount)]
+  return client.V1PodList(items=items)
+
+
+def MakeV1Node(name: str) -> client.V1Node:
+  """Make Kubernetes API Node response, see V1Node."""
+  return client.V1Node(metadata=MakeV1ObjectMeta(name=name))
+
+
+def MakeV1Pod(
+    name: Optional[str] = None,
+    namespace: Optional[str] = None,
+    node_name: Optional[str] = None,
+    labels: Optional[Labels] = None) -> client.V1Pod:
+  """Make Kubernetes API Pod response, see V1Pod."""
+  return client.V1Pod(
+      metadata=MakeV1ObjectMeta(name=name, namespace=namespace, labels=labels),
+      spec=client.V1PodSpec(node_name=node_name, containers=[]))
+
+
+def MakeV1PodTemplateSpec(labels: Labels) -> client.V1PodTemplateSpec:
+  """Make Kubernetes API template spec response, see V1PodTemplateSpec."""
+  return client.V1PodTemplateSpec(metadata=MakeV1ObjectMeta(labels=labels))
+
+
+def MakeV1ReplicaSet(
+    name: Optional[str] = None,
+    namespace: Optional[str] = None,
+    template_spec_labels: Optional[Labels] = None) -> client.V1ReplicaSet:
+  """Make Kubernetes API ReplicaSet response, V1ReplicaSet."""
+  return client.V1ReplicaSet(
+      metadata=MakeV1ObjectMeta(name=name, namespace=namespace),
+      spec=client.V1ReplicaSetSpec(
+          selector=client.V1LabelSelector(),
+          template=MakeV1PodTemplateSpec(template_spec_labels)))
+
+
+def MakeV1Deployment(
+    name: Optional[str] = None,
+    namespace: Optional[str] = None,
+    template_spec_labels: Optional[Labels] = None,
+    match_labels: Optional[Labels] = None) -> client.V1Deployment:
+  """Make Kubernetes API response deployment, see V1Deployment."""
+  return client.V1Deployment(
+      metadata=MakeV1ObjectMeta(name=name, namespace=namespace),
+      spec=client.V1DeploymentSpec(
+          selector=client.V1LabelSelector(match_labels=match_labels),
+          template=MakeV1PodTemplateSpec(template_spec_labels)))
+
+
+def MakeMockK8sPod(
+    name: str, namespace: str, read_response: client.V1Pod) -> base.K8sPod:
+  """Make mock Kubernetes Pod by patching Read method."""
   mock_pod = base.K8sPod(MOCK_API_CLIENT, name, namespace)
   mock_pod.Read = mock.Mock()
   mock_pod.Read.return_value = read_response
   return mock_pod
+
+
+def MakeMockK8sDeployment(
+    name: str, namespace: str,
+    read_response: client.V1Deployment) -> workloads.K8sDeployment:
+  """Make mock Kubernetes Deployment by patching Read method."""
+  mock_deploy = workloads.K8sDeployment(MOCK_API_CLIENT, name, namespace)
+  mock_deploy.Read = mock.Mock()
+  mock_deploy.Read.return_value = read_response
+  return mock_deploy
--- tests/providers/kubernetes/test_workloads.py
+++ tests/providers/kubernetes/test_workloads.py
@@ -36,7 +36,7 @@ class K8sWorkloadTest(unittest.TestCase):
     # Override abstract method
     mock_pod_match_labels.return_value = self.mock_match_labels
 
-    mock_pod_response = k8s_mocks.MakeMockPod(labels=self.mock_match_labels)
+    mock_pod_response = k8s_mocks.MakeV1Pod(labels=self.mock_match_labels)
     mock_pod = k8s_mocks.MakeMockK8sPod('name', 'namespace', mock_pod_response)
 
     workload = workloads.K8sWorkload(k8s_mocks.MOCK_API_CLIENT, 'name', 'namespace')
@@ -50,7 +50,7 @@ class K8sWorkloadTest(unittest.TestCase):
     # Override abstract method
     mock_pod_match_labels.return_value = self.mock_match_labels
 
-    mock_pod_response = k8s_mocks.MakeMockPod(labels={})
+    mock_pod_response = k8s_mocks.MakeV1Pod(labels={})
     mock_pod = k8s_mocks.MakeMockK8sPod('name', 'namespace', mock_pod_response)
 
     workload = workloads.K8sWorkload(k8s_mocks.MOCK_API_CLIENT, 'name', 'namespace')
@@ -64,7 +64,7 @@ class K8sWorkloadTest(unittest.TestCase):
     # Override abstract method
     mock_pod_match_labels.return_value = self.mock_match_labels
 
-    mock_pod_response = k8s_mocks.MakeMockPod(labels=self.mock_match_labels)
+    mock_pod_response = k8s_mocks.MakeV1Pod(labels=self.mock_match_labels)
     mock_pod = k8s_mocks.MakeMockK8sPod('name', 'production', mock_pod_response)
 
     workload = workloads.K8sWorkload(k8s_mocks.MOCK_API_CLIENT, 'name', 'namespace')
@@ -89,7 +89,7 @@ class K8sWorkloadTest(unittest.TestCase):
   @mock.patch.object(workloads.K8sWorkload, '_PodMatchLabels')
   @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
   def testListPodWithWorkloadLabels(self, mock_list_pod, mock_pod_match_labels):
-    """Tests that workload pods are listed in the same namespace."""
+    """Tests that workload pods are listed with the correct labels."""
     # Override abstract method
     mock_pod_match_labels.return_value = self.mock_match_labels
 
@@ -97,3 +97,41 @@ class K8sWorkloadTest(unittest.TestCase):
     workload.GetCoveredPods()
 
     self.assertEqual(mock_list_pod.call_args.kwargs['label_selector'], 'app=nginx-klzkdoho')
+
+  @mock.patch('kubernetes.client.AppsV1Api.delete_namespaced_replica_set')
+  def testReplicaSetNonCascadingDeletionArgs(self, mock_delete):
+    """Tests that a ReplicaSet's deletion specifies correct args."""
+    workload = workloads.K8sReplicaSet(k8s_mocks.MOCK_API_CLIENT, 'name-cvjcofvh', 'namespace-gonnujrl')
+    workload.Delete(cascade=False)
+    mock_delete.assert_called_once_with('name-cvjcofvh', 'namespace-gonnujrl', body={'propagationPolicy': 'Orphan'})
+
+  @mock.patch('kubernetes.client.AppsV1Api.delete_namespaced_deployment')
+  def testDeploymentNonCascadingDeletionArgs(self, mock_delete):
+    """Tests that a Deployment's deletion specifies correct args."""
+    workload = workloads.K8sDeployment(k8s_mocks.MOCK_API_CLIENT, 'name-cvjcofvh', 'namespace-gonnujrl')
+    workload.Delete(cascade=False)
+    mock_delete.assert_called_once_with('name-cvjcofvh', 'namespace-gonnujrl', body={'propagationPolicy': 'Orphan'})
+
+  @mock.patch('kubernetes.client.AppsV1Api.list_namespaced_replica_set')
+  def testDeploymentReplicaSet(self, mock_list_replica_set):
+    """Tests that a ReplicaSet matching the Deployment's spec is returned."""
+    mock_list_replica_set_response = mock.Mock()
+    mock_list_replica_set_response.items = [
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-0', template_spec_labels={}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-1', template_spec_labels={}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-2', template_spec_labels={}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-3', template_spec_labels={}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-4', template_spec_labels={'app': 'nginx', 'pod-template-hash': 'abcd'}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-5', template_spec_labels={}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-6', template_spec_labels={}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-7', template_spec_labels={}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-8', template_spec_labels={}),
+      k8s_mocks.MakeV1ReplicaSet(name='replicaset-9', template_spec_labels={}),
+    ]
+
+    mock_list_replica_set.return_value = mock_list_replica_set_response
+
+    deployment_response = k8s_mocks.MakeV1Deployment(template_spec_labels={'app': 'nginx'}, match_labels={'app': 'nginx'})
+    deployment = k8s_mocks.MakeMockK8sDeployment('deployment', 'default', deployment_response)
+
+    self.assertEqual(deployment._ReplicaSet().name, 'replicaset-4')
