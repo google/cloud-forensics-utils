a3f46b09c5456073234e86d7d1bae90547fb619f
Jonathan Greig <jonathangreig@google.com>
2022-08-30 17:02:34 +1000

    Broken Azure unit test (#456)
    * Broken Azure unit test
    Fixes #455

--- libcloudforensics/providers/azure/internal/compute_base_resource.py
+++ libcloudforensics/providers/azure/internal/compute_base_resource.py
@@ -16,7 +16,9 @@
 
 from typing import Optional, List, TYPE_CHECKING
 
-from azure.mgmt import compute as compute_sdk  # pylint: disable=import-error
+# pylint: disable=import-error
+from azure.mgmt import compute as compute_sdk # type: ignore
+# pylint: enable=import-error
 
 from libcloudforensics import errors
 from libcloudforensics.providers.azure.internal import common
