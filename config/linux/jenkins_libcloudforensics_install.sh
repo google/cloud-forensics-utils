2d21dc51de5b1b6898147453d2a8d82028ac4982
Fryyyyy <Fryyyyy@users.noreply.github.com>
2023-08-28 09:38:21 +1000

    Python -> Python3

--- config/linux/jenkins_libcloudforensics_install.sh
+++ config/linux/jenkins_libcloudforensics_install.sh
@@ -8,4 +8,4 @@ sudo apt-get install -y python3-pip
 sudo pip3 install poetry
 
 # Install libcloudforensics pinned requirements
-sudo python -m poetry install --with dev
+sudo python3 -m poetry install --with dev
