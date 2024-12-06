0b4e8d8758c27ce7a39cfa816f49c53f51671a99
Fry <fryy@google.com>
2024-12-06 14:58:13 +1100

    OrgPolicy inside CRM

--- libcloudforensics/providers/gcp/internal/cloudresourcemanager.py
+++ libcloudforensics/providers/gcp/internal/cloudresourcemanager.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Google Cloud Resource Manager functionality."""
-from typing import TYPE_CHECKING, Dict, List, Any
+from typing import TYPE_CHECKING, Dict, List, Any, Optional
 from googleapiclient import errors as google_api_errors
 
 from libcloudforensics import logging_utils
@@ -164,3 +164,146 @@ class GoogleCloudResourceManager:
         resource_client, 'getIamPolicy', request)[0]
 
     return response
+
+  def GetOrgPolicy(self, resource, constraint: str) -> Dict[str, Any]:
+    """Gets a particular Org Policy on a resource.
+
+    Args:
+      resource (str): a resource identifier in the format
+        resource_type/resource_number e.g. projects/123456789012 where
+        project_type is one of projects, folders or organizations.
+      constraint (str): the name of the constraint to get.
+
+    Returns:
+      Dict[str, Any]: The Org Policy details.
+        See https://cloud.google.com/resource-manager/reference/rest/v1/Policy
+
+    Raises:
+      TypeError: if an invalid resource type is provided.
+    """
+    resource_type = resource.split('/')[0]
+    if resource_type not in self.RESOURCE_TYPES:
+      raise TypeError('Invalid resource type "{0:s}", resource must be one of '
+          '"projects", "folders" or "organizations" provided in the format '
+          '"resource_type/resource_number".'.format(resource))
+
+    if not constraint.startswith('constraints/'):
+      constraint = 'constraints/' + constraint
+
+    # Override API version, since this doesn't exist in v2 or v3
+    self.RESOURCE_MANAGER_API_VERSION = 'v1'
+    service = self.GrmApi()
+    resource_client = getattr(service, resource_type)()
+    response = resource_client.getOrgPolicy(
+        resource=resource, body={'constraint': constraint}
+    ).execute()
+    return response
+
+  def ListOrgPolicy(self, resource: str) -> Dict[str, Any]:
+    """Lists all Org Policies on a resource.
+
+    Args:
+      resource (str): a resource identifier in the format
+        resource_type/resource_number e.g. projects/123456789012 where
+        project_type is one of projects, folders or organizations.
+
+    Returns:
+      Dict[str, Any]: The Org Policy details.
+        See https://cloud.google.com/resource-manager/reference/rest/v1/Policy
+
+    Raises:
+      TypeError: if an invalid resource type is provided.
+    """
+    resource_type = resource.split('/')[0]
+    if resource_type not in self.RESOURCE_TYPES:
+      raise TypeError('Invalid resource type "{0:s}", resource must be one of '
+          '"projects", "folders" or "organizations" provided in the format '
+          '"resource_type/resource_number".'.format(resource))
+
+    # Override API version, since this doesn't exist in v2 or v3
+    self.RESOURCE_MANAGER_API_VERSION = 'v1'
+    service = self.GrmApi()
+    resource_client = getattr(service, resource_type)()
+    response = resource_client.listOrgPolicies(resource=resource).execute()
+    return response
+
+  def SetOrgPolicy(
+      self, resource: str, policy: Dict[str, Any],
+      etag: Optional[str] = None) -> Dict[str, Any]:
+    """Updates the specified Policy on the resource.
+    Creates a new Policy for that Constraint on the resource if one does not exist.
+    
+
+    Args:
+      resource (str): a resource identifier in the format
+        resource_type/resource_number e.g. projects/123456789012 where
+        project_type is one of projects, folders or organizations.
+      policy (dict): The policy to create, as per
+        https://cloud.google.com/resource-manager/reference/rest/v1/Policy
+      etag (str): The current version, for concurrency control.
+        Not supplying an etag on the request Policy results in an unconditional
+        write of the Policy.
+
+    Returns:
+      Dict[str, Any]: The Org Policy that was created.
+        https://cloud.google.com/resource-manager/reference/rest/v1/Policy
+
+    Raises:
+      TypeError: if an invalid resource type is provided.
+    """
+    resource_type = resource.split('/')[0]
+    if resource_type not in self.RESOURCE_TYPES:
+      raise TypeError('Invalid resource type "{0:s}", resource must be one of '
+          '"projects", "folders" or "organizations" provided in the format '
+          '"resource_type/resource_number".'.format(resource))
+
+    # Override API version, since this doesn't exist in v2 or v3
+    self.RESOURCE_MANAGER_API_VERSION = 'v1'
+    service = self.GrmApi()
+    resource_client = getattr(service, resource_type)()
+    body = {'policy': policy}
+    if etag:
+      body['policy']['etag'] = etag
+    response = resource_client.setOrgPolicy(resource=resource,
+                                            body=body).execute()
+    return response
+
+  def DeleteOrgPolicy(
+      self, resource, constraint: str, etag: Optional[str] = None) -> bool:
+    """Removes a particular Org Policy on a resource.
+
+    Args:
+      resource (str): a resource identifier in the format
+        resource_type/resource_number e.g. projects/123456789012 where
+        project_type is one of projects, folders or organizations.
+      constraint (str): the name of the constraint to get.
+      etag (str): The current version, for concurrency control.
+        Not sending an etag will cause the Policy to be cleared blindly.
+
+    Returns:
+      bool: True if successful, 
+
+    Raises:
+      TypeError: if an invalid resource type is provided.
+    """
+    resource_type = resource.split('/')[0]
+    if resource_type not in self.RESOURCE_TYPES:
+      raise TypeError('Invalid resource type "{0:s}", resource must be one of '
+          '"projects", "folders" or "organizations" provided in the format '
+          '"resource_type/resource_number".'.format(resource))
+
+    if not constraint.startswith('constraints/'):
+      constraint = 'constraints/' + constraint
+
+    # Override API version, since this doesn't exist in v2 or v3
+    self.RESOURCE_MANAGER_API_VERSION = 'v1'
+    service = self.GrmApi()
+    resource_client = getattr(service, resource_type)()
+    body = {'constraint': constraint}
+    if etag:
+      body['etag'] = etag
+    response = resource_client.clearOrgPolicy(resource=resource,
+                                              body=body).execute()
+    if not response:
+      return True
+    return False
